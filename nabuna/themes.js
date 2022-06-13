c1b7dc19909bd2f2d678586a9cabfa1ef3a4a47e
Adrian <adrianmontero@tutanota.com>
2022-06-13 19:18:39 +0200

    asd

---
+++ nabuna/themes.js
@@ -0,0 +1,11 @@
+const profile_wrappers = document.getElementsByClassName("profile-left-wrapper");
+
+function darkMode() {
+    document.body.style.backgroundColor = "black";
+    document.getElementsByTagName("div").style.backgroundColor = "blue";
+    document.b.style.color = "white";
+}
+
+function lightMode() {
+    document.body.style.backgroundColor = "white";
+}
